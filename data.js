b6375e1faa184651a1c2786cabf6612993779d30
Viktor Reiser <viktorreiser@gmx.de>
2014-04-28 16:26:14 +0200

    First documentation

--- data.js
+++ data.js
@@ -1,3 +1,108 @@
 epicdata = [
-  
+    [
+        "data",
+        [
+            {
+                "id": "145a7f148178cad6b31",
+                "type": "section",
+                "title": "EpicDoc",
+                "created": "2014-04-28T12:46:18"
+            },
+            {
+                "id": "145a7f197a576c0bda4",
+                "parentId": "145a7f148178cad6b31",
+                "type": "text",
+                "title": "What is EpicDoc?",
+                "created": "2014-04-28T12:46:39",
+                "contentmodified": "2014-04-28T16:06:23",
+                "content": [
+                    "## What is EpicDoc?",
+                    "",
+                    "EpicDoc is a standalone and universal documentation (tool).  It combines the produced documentation and the tools to write it in one application.",
+                    "",
+                    "**Note:** EpicDoc is in the initial phase right now and there's more to come. Right now you can't modify this documentation online but this will change in feature. (Great potential, too little human resource.)",
+                    "",
+                    "### How does it work?",
+                    "",
+                    "<img style=\"float:right; margin:0 0 10px 10px;\" src=\"images/epicdoc.png\" alt=\"How it works picture\" />",
+                    "",
+                    " * the documentation is readable anytime, you just need a web browser",
+                    " * `data.js` contains your whole documentation (besides custom resources you include, e.g. this picture)",
+                    " * if you want to modify the content you can start the `node.js web server` and documentation becomes live (we have to overcome some limitations with a local web server because a pure HTML application is not capable to do everything it wants to, especially filesystem operations)",
+                    "",
+                    "That's it. That's the basic idea. Everything else is built on top of that. Looks complicated but actually it's easy: Open `index.html` and read the content. If you want to modify the content, kick of the included server start.",
+                    ""
+                ]
+            },
+            {
+                "id": "145a8490c25aec300ba",
+                "parentId": "145a7f148178cad6b31",
+                "type": "text",
+                "title": "Changelog",
+                "created": "2014-04-28T14:22:10",
+                "contentmodified": "2014-04-28T16:02:41",
+                "content": [
+                    "## Changelog History",
+                    "",
+                    "### v0.1",
+                    "",
+                    " * `04/28/2014` first version, pretty alpha, we'll stay on this version until it's ready be used",
+                    ""
+                ]
+            },
+            {
+                "id": "145a85f4f0787d85f2f",
+                "parentId": "145a7f148178cad6b31",
+                "type": "text",
+                "title": "The very thing?",
+                "created": "2014-04-28T14:46:29",
+                "contentmodified": "2014-04-28T16:02:33",
+                "content": [
+                    "## Is EpicDoc what I was looking for?",
+                    "",
+                    "Maybe? EpicDoc is just another documentaion tool. But how to decide whether it's right for me?  ",
+                    "Here you'll find the inspiration and motivation behind EpicDoc and you can decide yourself if it fits your needs.  ",
+                    "The overall goal of EpicDoc is to overcome limits of other tools which finally sum up to the need of something else.  ",
+                    "",
+                    "### What I want",
+                    "",
+                    "* **Centralized information**  ",
+                    "I want to collect information about any kind of knowledge, experience, technology, etc. in one location so I can find and access it as efficient as possible whenever I need it.",
+                    "",
+                    "",
+                    "* **Live and not generated**  ",
+                    "I want to modify content live without the requirement to press preview instantly or even start some generator which produces a result I have to check and to see then that the actual content doesn't look the way I intended.",
+                    "",
+                    "",
+                    "* **Standalone and platform independent**  ",
+                    "I don't want to install and/or force someone else to install a piece of software to view my documentation. Everyone should be able to read the *same* content on his chosen platform. I want to be able to bring the content as it is into the web. ",
+                    "",
+                    "",
+                    "* **Easy to backup and move**  ",
+                    "I want to take my documentation copy it somewhere else and be ready to go (no databases dumps or something like that).",
+                    "",
+                    "",
+                    "* **Version control friendly**  ",
+                    "I'd like to commit the documentation to a VCS but I want it be text and also bundled to a single file (instead spreading it to full blown folder structure).",
+                    "",
+                    "",
+                    "### Conclusion",
+                    "",
+                    "Is it what you were looking for? If you're searching for a tool which provides a full blown documentation system which ties as itself as close as possible to your project then EpicDoc might not be the right thing.",
+                    "",
+                    "If I had to classify EpicDoc I would say:  ",
+                    "It's an enhanced and standalone wiki system based on web technologies without having the annoying drawback of being tied to a web server architecture. But it still leaves me the opportunity to bring the content to web very easily.",
+                    ""
+                ]
+            }
+        ]
+    ],
+    [
+        "dataorder",
+        [
+            "145a7f197a576c0bda4",
+            "145a85f4f0787d85f2f",
+            "145a8490c25aec300ba"
+        ]
+    ]
 ]
